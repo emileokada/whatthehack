71dea9647bd16268cd045ad73e45d039ef5c954e
Emile Okada <emileokada@gmail.com>
2017-09-05 21:17:13 +0200

    Added side panel and revenue & popularity scores

--- scraper/serve_ratings.py
+++ scraper/serve_ratings.py
@@ -1,11 +1,16 @@
 import json
-from api_functions import get_transactions
+#from api_functions import get_pub_transactions
 
-with open('pub_account_data.json') as f:
-    pubs = json.load(f)
+with open('pub_transactions.json') as f:
+    pub_transactions = json.load(f)
 
-for pub in pubs:
-    transactions = get_pub_transactions(pub)
-    print(transactions)
-    #pub['number_of_transactions'] = len(transactions)
-    #pub['revenue'] = sum((transaction['amount'] for transaction in transactions)
+def get_pub_score(pub_acc):
+    if pub_acc in pub_transactions:
+        transactions = pub_transactions[pub_acc]
+        revenue = sum(transaction['amount'] for transaction in transactions)
+        number_of_transactions = len(transactions)
+    else:
+        revenue = 0
+        number_of_transactions = 0
+    return revenue, number_of_transactions
+
